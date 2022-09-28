c5b6960b2d705feeca007d1a3469cf0ef0b6e652
pereiracaslu <lucascosta2512@gmail.com>
2022-09-28 13:29:26 -0300

    Criação das components

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";  
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+           <h1 className="titulo">Trikas</h1>
+           <img src="https://i.pinimg.com/564x/d4/59/bf/d459bfde406a0370cfead0ae54258adc.jpg" alt="Trikas"
+           className="img"/>
+     
+        </>
+    )
+}
+
+export default Home;
