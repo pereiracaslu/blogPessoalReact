9b33d75db44c58d08eded97ead3b6c26e45b1999
pereiracaslu <lucascosta2512@gmail.com>
2022-09-29 13:01:33 -0300

    Desenvolvimento da pasta components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import Navbar from './components/estaticos/navbar/Navbar'
+import Footer from './components/estaticos/footer/Footer'
+import { Grid } from '@material-ui/core';
 import './App.css';
 import Home from './paginas/home/Home';
 
-let nome = "Lucas Pereira"
+
 function App() {
   return (
     <>
-     <h1><Home/></h1>
-    
+   <Navbar/>
+   <Home/>
+   <Footer/>
     </>
-  
   );
 }
-
 export default App;
