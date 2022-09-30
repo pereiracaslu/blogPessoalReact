93ce9f3b9849a84475499f4a33312dc8eeb04fc6
pereiracaslu <lucascosta2512@gmail.com>
2022-09-30 15:56:26 -0300

    Estilizando o login

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar'
 import Footer from './components/estaticos/footer/Footer'
-import { Grid } from '@material-ui/core';
+import Login from './paginas/login/Login';
 import './App.css';
 import Home from './paginas/home/Home';
 
-
 function App() {
   return (
-    <>
-   <Navbar/>
-   <Home/>
-   <Footer/>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </Router>
+
   );
 }
 export default App;
